4923ddb9a06c249568b8af49f1fc4d8f29b98a8c
albnsp <a.suprunova1@gmail.com>
2023-11-22 20:48:47 +0400

    add Task01.java

---
+++ src/Task01.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Task01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите количество строк треугольника: ");
+        int rows = scanner.nextInt();
+
+// Заполненный треугольник
+        System.out.println("Заполненный треугольник:");
+        for (int i = 1; i <= rows; i++) {
+            for (int j = rows; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+// Пустой треугольник
+        System.out.println("\nПустой треугольник:");
+        for (int i = 1; i <= rows; i++) {
+            for (int j = rows; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                if (i == rows || k == 1 || k == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
